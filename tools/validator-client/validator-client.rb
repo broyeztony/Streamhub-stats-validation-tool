f4fa914506fbdc2e1273293961d041da9c6a8eaf
Takahiro Yoshihara <takahiro@fivecool.net>
2016-04-11 22:00:52 +0900

    Fix IOError with Ruby 2.0.0 :/
    This is dirty fixes, so code is messy

--- tools/validator-client/validator-client.rb
+++ tools/validator-client/validator-client.rb
@@ -1,4 +1,5 @@
 #! /usr/bin/env ruby
+# coding: utf-8
 
 require "bundler"
 Bundler.require
@@ -21,9 +22,18 @@ def api
   end
 end
 
-if ARGV.size < 1
-  puts "Usage: #$0 INPUT-FILE"
-  exit 1
+opts = {}
+OptionParser.new do |o|
+  o.banner = "Usage: #{File.basename($0)} INPUT-FILE [OPTIONS]"
+  o.on("-a", "--all") { |v| opts[:all] = v  }
+  o.on("-o", "--ok-only") { |v| opts[:ok] = v  }
+  o.on("-k", "--ko-only") { |v| opts[:ko] = v  }
+  o.parse!(ARGV)
+
+  if ARGV.size < 1
+    puts o.banner
+    exit 1
+  end
 end
 
 infile = ARGV.first
@@ -44,13 +54,16 @@ rescue Faraday::ConnectionFailed => e
 end
 
 @headers = {}
-CSV.foreach(open(infile), col_sep: "\t").first.each do |c|
+File.readlines(open(infile)).first.split("|").each do |c|
+# CSV.foreach(open(infile), col_sep: "|").first.each do |c|
+  next if c.chomp.size == 0
   @headers[c.downcase.intern] = c
 end
 
-@table = CSV.table(open(infile), headers: :first_row, converters: :integer, col_sep: "\t", skip_blanks: true, skip_lines: /^#/)
+# @table = CSV.table(open(infile), headers: :first_row, col_sep: "\t", skip_blanks: true, skip_lines: /^#/)
+@table = CSV.table(open(infile), col_sep: "|")
 
-out = []
+buf = []
 
 @table.each_with_index do |row, index|
   expected = ""
@@ -59,7 +72,7 @@ out = []
   begin
     res = conn.get do |req|
       req.url api
-      expected = row[:_expected]
+      expected = row[:_expected] == "OK"
 
       @headers.each do |k,v|
         next unless row[k]
@@ -71,22 +84,30 @@ out = []
       params = req.params
     end
   rescue Faraday::ConnectionFailed
-    out << { "error" => "Failed to connect to #{url}" }
+    buf << { "error" => "Failed to connect to #{url}" }
     break
   end
 
   if res.status == 200
     j = JSON.load(res.body)
-    j["id"] = index
-    j["params"] = params
+    j = { "id" => index }.merge(j)
+    # j["params"] = params
     j["_expected"] = expected
-    # this is not supported yet
-    # j["result"] = expected == j["actual"]
-    out << j
+    j["result"] = (expected == j["valid"] && j["error"].nil?) ? "OK" : "NG"
+    buf << j
   else
     # Internal Server Error or something similar
-    out << { "error" => res.body, "id" => index, "params" => params, "_expected" => expected }
+    buf << { "error" => res.body, "id" => index, "params" => params, "_expected" => expected }
   end
 end
 
-puts JSON.dump(out)
+output = case
+when opts[:all]
+  JSON.dump(buf)
+when opts[:ok]
+  JSON.dump(buf.select { |e| e["result"] == "OK" })
+else
+  JSON.dump(buf.select { |e| e["result"] == "NG" })
+end
+
+puts output
